977b7b944f80bfbed68327cc65eb290fcf82b822
Tracie Ling Yan Ying <57853991+tracielyy@users.noreply.github.com>
2026-04-04 17:07:39 +0800

    Feature: allow link to company website for experience section

--- src/components/sections/ExperienceSection.js
+++ src/components/sections/ExperienceSection.js
@@ -2,6 +2,7 @@ import { getDictionary } from '@/lib/dictionary';
 import { BadgesDisplay } from '@/components/ui/Badge';
 import { Experience } from '@/lib/models/Experience.class';
 import { PiArrowFatLinesUpBold } from "react-icons/pi";
+import { FaExternalLinkAlt } from "react-icons/fa";
 
 export default function ExperienceSection({ locale }) {
     const experiences = Experience.getAllExperiences(locale);
@@ -15,15 +16,18 @@ export default function ExperienceSection({ locale }) {
                 {
                     experiences.map((experience) => (
                         <div key={experience.id} className='container'>
-                            
+
                             <div className='experience-card'>
                                 <span className="text-gray-500 text-sm">{experience.displayExperienceDuration()}</span>
                                 <span className='experience-job-title pt-2 pb-3 block'>
                                     <b>
                                         {experience.jobTitle} @ &nbsp;
-                                        <span className='text-accent-secondary'>
-                                            {experience.organisation}
-                                        </span>
+                                        <a href={experience.website} target="_blank">
+                                            <span className='text-accent-secondary inline-flex items-center gap-2 no-underline hover:underline hover:decoration-accent-secondary'>
+                                                {experience.organisation}
+                                                <FaExternalLinkAlt size={12}/>
+                                            </span>
+                                        </a>
                                     </b>
                                 </span>
 
